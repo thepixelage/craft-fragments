28361da78744251f76fd17f87ed3b83b2954bb94
ronlim <ron.lim@thepixelage.com>
2021-08-04 13:46:30 +0800

    Start from clean slate

--- src/config/routes.php
+++ src/config/routes.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'fragments/settings' => 'fragments/fragments/settings-index',
-    'fragments/settings/types' => 'fragments/fragment-types/index',
-    'fragments/settings/types/new' => 'fragments/fragment-types/edit',
-    'fragments/settings/types/<typeId:\d+>' => 'fragments/fragment-types/edit',
-    'fragments/settings/zones' => 'fragments/zones/index',
-    'fragments/settings/zones/new' => 'fragments/zones/edit',
-    'fragments/settings/zones/<zoneId:\d+>' => 'fragments/zones/edit',
-
-    'fragments' => 'fragments/fragments/fragment-index',
-    'fragments/<fragmentTypeHandle:{handle}>' => 'fragments/fragment-index',
+//    'fragments/settings/types' => 'fragments/fragment-types/index',
+//    'fragments/settings/types/new' => 'fragments/fragment-types/edit',
+//    'fragments/settings/types/<typeId:\d+>' => 'fragments/fragment-types/edit',
+//    'fragments/settings/zones' => 'fragments/zones/index',
+//    'fragments/settings/zones/new' => 'fragments/zones/edit',
+//    'fragments/settings/zones/<zoneId:\d+>' => 'fragments/zones/edit',
+//    'fragments/fragments' => 'fragments/fragments/index',
+//    'fragments/fragments/<fragmentTypeHandle:{handle}>' => 'fragments/fragments/index',
+//    'fragments/fragments/<fragmentTypeHandle:{handle}>/new' => 'fragments/fragments/edit',
+//    'fragments/fragments/<fragmentTypeHandle:{handle}>/<fragmentId:\d+>' => 'fragments/fragments/edit',
 ];
